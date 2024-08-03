ca5ae2f309be19b0fcf0a1fa583f31128a664f1f
Mohan-Dos <mohan638010@gmail.com>
2024-08-03 08:31:00 -0700

    add content

--- emailsend/mail/views.py
+++ emailsend/mail/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 # Create your views here.
+def success(request):
+
+    return render(request,'success.html')
+
+def send_email(request):
+
+    context = {
+        'mymail' : settings.EMAIL_HOST_USER
+    }
+        
+    if request.method == 'POST':
+    
+        subject = request.POST['subject']
+        message = request.POST['message']
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [request.POST['mail']]
+        
+        send_mail(subject, message, email_from, recipient_list)
+
+        return redirect('http://127.0.0.1:8000/success/')
+    
+    return render(request,'send_email.html',context)
+    
--- emailsend/mail_send/settings.py
+++ emailsend/mail_send/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mail',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mohan638010@gmail.com'
+EMAIL_HOST_PASSWORD = 'hojczepgjwdlfiwx'
